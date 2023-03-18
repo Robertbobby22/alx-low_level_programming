1d9cb500332049df0ca5db986f577754b9470f2c
Robertbobby22 <robertnduva02@gmail.com>
2023-03-17 23:54:25 -0700

    program that prints the alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,14 @@
  *
  * Return: 0
  */
+
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	
-	for i = 0; i < 26; i++)
-	{
-		putchar(alp[i])
-	}
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
-}	
+}
