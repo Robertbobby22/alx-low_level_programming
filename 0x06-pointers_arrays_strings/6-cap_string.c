ca32e86907826f6e8666d19fe49ae8ed51484294
Robertbobby <robertnduva02@gmail.com>
2023-03-30 15:22:05 -0700

    Mozart composed his music not for the elite but for everybody

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: string to be capitalized
+ *
+ * Return: result
+ */
+char *cap_string(char *)
+{
+	int digit = 0;
+
+	while (!(str[digit] >= 'a' && str[digit] <= 'z'))
+		digit++;
+
+	if (str[index - 1] == ' ' ||
+	
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @str: string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+char *leet(char *str)
+{
+	int digx1 = 0, digx2;
+	char leet[8] = {'0', 'l', '?', 'E', 'A', '?', '?', 'J'};
+
+	while (str[digx1])
+	{
+		for (digx2 = 0; digx2 <= 7; digx2++)
+		{
+			if (str[digx1] == leet[digx2] || str[digx1] - 32 == leet[digx2])
+				str[digx1] = digx2 + '0';
+		}
+
+		digx1++;
+	}
+	return (str);
+}
