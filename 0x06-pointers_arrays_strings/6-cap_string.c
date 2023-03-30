6005e43a5f7cfb98930621d10bbd3f6b45ab7b6e
Robertbobby <robertnduva02@gmail.com>
2023-03-30 16:02:23 -0700

    Expect the best prepare the worst capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,39 @@
 #include "main.h"
 
 /**
- * cap_string - function that capitalizes all words of a string
+ * cap_string - function that capitalizes
+ * all words of a string
  * @str: string to be capitalized
  *
  * Return: result
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int digit = 0;
 
-	while (!(str[digit] >= 'a' && str[digit] <= 'z'))
+	while (str[digit])
+	{
+		while (!(str[digit] >= 'a' && str[digit] <= 'z'))
 		digit++;
 
-	if (str[index - 1] == ' ' ||
-	
+	if (str[digit - 1] == ' ' ||
+		str[digit - 1] == '\t' ||
+		str[digit - 1] == '\n' ||
+		str[digit - 1] == ',' ||
+		str[digit - 1] == ';' ||
+		str[digit - 1] == '.' ||
+		str[digit - 1] == '!' ||
+		str[digit - 1] == '?' ||
+		str[digit - 1] == '~' ||
+		str[digit - 1] == '(' ||
+		str[digit - 1] == ')' ||
+		str[digit - 1] == '{' ||
+		str[digit - 1] == '}' ||
+		digit == 0)
+		str[digit] -= 32;
+
+	digit++;
+	}
+
+	return (str);
+}
